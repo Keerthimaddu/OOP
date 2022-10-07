e54bb7e264fd87e8516a86d86958582d147a83e9
Keerthimaddu <101540034+Keerthimaddu@users.noreply.github.com>
2022-10-07 09:01:39 +0530

    Create explicit_type_casting.cpp

---
+++ explicit_type_casting.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	int intvar=25;
+	float floatvar=25.87;
+	cout<<"intvar="<<intvar;
+	cout<<"\n float var="<<floatvar;
+	cout<<"\n float(intvar=)"<<float(intvar);
+	cout<<"\n int(floatvar=)"<<int(floatvar);
+	getch();
+	}
