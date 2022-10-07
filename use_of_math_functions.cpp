a4a51e1476c6f7fc40272f7164f9673cddda5ced
Keerthimaddu <101540034+Keerthimaddu@users.noreply.github.com>
2022-10-07 09:48:15 +0530

    Create use_of_math_functions.cpp

---
+++ use_of_math_functions.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<conio.h>
+#include<iomanip>
+#include<math.h>
+using namespace std;
+int main()
+{ 
+  cout<<fixed<<setprecision(2);
+  cout<<"sin (100) = "<<sin(100.00)<<"\n";               
+  cout<<"cos (100) = "<<cos(100.00)<<"\n";             
+  cout<<"tan (100) = "<<tan(100.00)<<"\n";            
+  cout<<"7 to the power of 6 = "<<pow(7.0,6.0)<<"\n"; 
+  cout<<"log10 (10) = "<<log10(10.00)<<"\n";        
+  cout<<"sqrt (2) = "<<sqrt (2.00)<<"\n";           
+  
+  
+  getch ();
+  return 0;
+  
+};
+
+output:
+sin (100) = -0.51
+cos (100) = 0.86
+tan (100) = -0.59
+7 to the power of 6 = 117649.00
+log10 (10) = 1.00
+sqrt (2) = 1.41
